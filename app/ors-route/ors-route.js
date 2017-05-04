d8cd42cd71ba18ac7221477804f9507fa90c852c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-04 16:29:43 +0200

    Add: ui-router

---
+++ app/ors-route/ors-route.js
@@ -0,0 +1,39 @@
+(function () {
+	'use strict';
+
+	var app = angular.module('ors-route', ['ui.router']);
+
+	app.config(function ($stateProvider) {
+		var homeState = {
+			name: 'home',
+			url: '/',
+			templateUrl: './ors-route/tmpl/home.html'
+		};
+
+		var productState = {
+			name: 'product',
+			url: '/products',
+			templateUrl: './ors-route/tmpl/product.html'
+		};
+
+		var serviceState = {
+			name: 'service',
+			url: '/services',
+			templateUrl: './ors-route/tmpl/service.html'
+		};
+
+		var contactState = {
+			name: 'contact',
+			url: '/contact',
+			templateUrl: './ors-route/tmpl/contact.html'
+		};
+
+		$stateProvider.state(homeState);
+		$stateProvider.state(productState);
+		$stateProvider.state(serviceState);
+		$stateProvider.state(contactState);
+	});
+
+
+
+})();
