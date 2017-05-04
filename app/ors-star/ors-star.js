d501e47eeef67dcde1b3f7799aea5364adf28314
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-04 13:26:36 +0200

    add: ors-star2

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -36,4 +36,38 @@
 		};
 	});
 
+	app.directive('orsStar2', function () {
+		return {
+			restrict: 'E',
+			scope: {
+				note: '<?'
+			},
+			controller: function ($scope, $element, $compile) {
+				'ngInject';
+				var ctrl = this;
+				console.log('orsStar2 ctrl', arguments);
+				$scope.$watch('note', function () {
+					var note = $scope.note || 0;
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
+					var html = '';
+					for (var i = 0; i < note; i++) {
+						html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png" />';
+					}
+					for (i = note; i < 5; i++) {
+						html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./ors-star/img/white_star.png" />';
+					}
+					$element.html(html);
+					$compile($element.contents())($scope);
+				});
+				ctrl.update = function (note) {
+					console.log('update', arguments);
+					$scope.note = note;
+				};
+
+			},
+			controllerAs: '$ctrl'
+		};
+	});
+
 })();
