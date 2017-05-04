00cb251e559bdeee383bdc370bf798d513dae698
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-04 12:16:42 +0200

    directive avec ng-click

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -7,9 +7,9 @@
 		return {
 			restrict: 'E',
 			scope: {
-				note: '=?'
+				note: '<?'
 			},
-			template: '<span><img ng-repeat="i in [0,1,2,3,4]" ng-src="{{$ctrl.getSrc(i)}}" alt="">' +
+			template: '<span><img ng-click="$ctrl.update(i+1)" ng-repeat="i in [0,1,2,3,4]" ng-src="{{$ctrl.getSrc(i)}}" alt="">' +
 				'</span>',
 			controller: function ($scope) {
 				var ctrl = this;
@@ -18,14 +18,19 @@
 				$scope.$watch('note', function () {
 					note = $scope.note || 0;
 				});
-				
+
 				console.log('note', note);
 				ctrl.getSrc = function (i) {
+					// console.log('getSrc', arguments);
 					if (i < note) {
 						return './ors-star/img/yellow_star.png';
 					}
 					return './ors-star/img/white_star.png';
-				}
+				};
+				ctrl.update = function (note) {
+					console.log('update', arguments);
+					$scope.note = note;
+				};
 			},
 			controllerAs: '$ctrl'
 		};
