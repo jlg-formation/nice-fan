a28f979b926ad86cc1f95910d823e3d03e2c1c23
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-04 13:37:44 +0200

    add: remove ctrlAs

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -9,7 +9,8 @@
 			scope: {
 				note: '<?'
 			},
-			template: '<span><img ng-click="$ctrl.update(i+1)" ng-repeat="i in [0,1,2,3,4]" ng-src="{{$ctrl.getSrc(i)}}" alt="">' +
+			template: '<span><img ng-click="$ctrl.update(i+1)" ' +
+				'ng-repeat="i in [0,1,2,3,4]" ng-src="{{$ctrl.getSrc(i)}}" alt="">' +
 				'</span>',
 			controller: function ($scope) {
 				var ctrl = this;
@@ -36,38 +37,34 @@
 		};
 	});
 
-	app.directive('orsStar2', function () {
-		return {
-			restrict: 'E',
-			scope: {
-				note: '<?'
-			},
-			controller: function ($scope, $element, $compile) {
-				'ngInject';
-				var ctrl = this;
-				console.log('orsStar2 ctrl', arguments);
-				$scope.$watch('note', function () {
-					var note = $scope.note || 0;
-					note = (note > 5) ? 5 : note;
-					note = (note < 0) ? 0 : note;
-					var html = '';
-					for (var i = 0; i < note; i++) {
-						html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png" />';
-					}
-					for (i = note; i < 5; i++) {
-						html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./ors-star/img/white_star.png" />';
-					}
-					$element.html(html);
-					$compile($element.contents())($scope);
-				});
-				ctrl.update = function (note) {
-					console.log('update', arguments);
-					$scope.note = note;
-				};
+	app.component('orsStar2', {
+		bindings: {
+			note: '<?'
+		},
+		controller: function ($scope, $element, $compile) {
+			'ngInject';
+			var ctrl = this;
+			console.log('orsStar2 ctrl', arguments);
+			$scope.$watch('$ctrl.note', function () {
+				var note = ctrl.note || 0;
+				note = (note > 5) ? 5 : note;
+				note = (note < 0) ? 0 : note;
+				var html = '';
+				for (var i = 0; i < note; i++) {
+					html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./ors-star/img/yellow_star.png" />';
+				}
+				for (i = note; i < 5; i++) {
+					html += '<img ng-click="$ctrl.update(' + (i + 1) + ')" src="./ors-star/img/white_star.png" />';
+				}
+				$element.html(html);
+				$compile($element.contents())($scope);
+			});
+			ctrl.update = function (note) {
+				console.log('update', arguments);
+				ctrl.note = note;
+			};
 
-			},
-			controllerAs: '$ctrl'
-		};
+		}
 	});
 
 })();
