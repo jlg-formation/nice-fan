80de6ae3c3feeee124d8d31ff1bee4e7fa63c73e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-04 11:41:14 +0200

    ors-star derective

--- app/ors-star/ors-star.js
+++ app/ors-star/ors-star.js
@@ -6,8 +6,22 @@
 	app.directive('orsStar', function () {
 		return {
 			restrict: 'E',
-			template: '<span><img ng-repeat="i in [0,1,2]" src="./ors-star/img/yellow_star.png" alt="">' +
-				'<img ng-repeat="i in [3,4]" src="./ors-star/img/white_star.png" alt=""></span>',
+			scope: true,
+			template: '<span><img ng-repeat="i in [0,1,2,3,4]" ng-src="{{$ctrl.getSrc(i)}}" alt="">' +
+				'</span>',
+			controller: function ($attrs) {
+				var ctrl = this;
+				console.log('orsStar ctrl', arguments);
+				var note = ('note' in $attrs) ? $attrs.note : 3;
+				console.log('note', note);
+				ctrl.getSrc = function (i) {
+					if (i < note) {
+						return './ors-star/img/yellow_star.png';
+					}
+					return './ors-star/img/white_star.png';
+				}
+			},
+			controllerAs: '$ctrl'
 		};
 	});
 
