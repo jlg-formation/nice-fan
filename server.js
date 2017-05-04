c033b93b61164de3cb809cc5325e2708c393ba3a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-04 16:54:08 +0200

    add: url rewritting

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-(function() {
-	'use strict';
+'use strict';
 
-	var express = require('express'); // charge ExpressJS
-	var serveIndex = require('serve-index');
+var express = require('express'); // charge ExpressJS
+var serveIndex = require('serve-index');
 
-	var app = express();
+var app = express();
 
-	app.use(express.static('.'));
-	app.use(serveIndex('.', {icons: true}));
+app.use(express.static('.'));
+app.use(serveIndex('.', {
+	icons: true
+}));
 
-	app.use(function(req, res, next) {
-		console.log('404: Page not Found', req.url);
-		next();
+app.use(['/app/products', '/app/services', '/app/contact'], function (req, res, next) {
+	res.sendFile('./app/index.html', {
+		root: __dirname
 	});
+});
 
-	app.listen(8000, function() {
-		console.log('server started on port 8000');
-	});
-})();
+app.use(function (req, res, next) {
+	console.log('404: Page not Found', req.url);
+	next();
+});
+
+app.listen(8000, function () {
+	console.log('server started on port 8000');
+});
