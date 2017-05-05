d38beaa9e0f4fe96f7e607ebbfd8b6fa82254748
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-05-05 10:52:20 +0200

    http get

--- app/ors-route/ProductCtrl.js
+++ app/ors-route/ProductCtrl.js
@@ -3,10 +3,16 @@
 
 	var app = angular.module('ors-route');
 
-	app.controller('ProductCtrl', function ProductCtrl(){
+	app.controller('ProductCtrl', function ProductCtrl($http) {
+		'ngInject';
 		var ctrl = this;
-		ctrl.start = function() {
+		ctrl.start = function () {
 			console.log('Start', arguments);
+			$http.get('/ws/s1').then((response) => {
+				console.log('response', response);
+			}).catch((error) => {
+				console.error('error', error);
+			});
 		};
 	});
 })();
